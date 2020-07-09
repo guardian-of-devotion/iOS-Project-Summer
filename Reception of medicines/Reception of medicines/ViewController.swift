c615286e605ec486efe562a75274229d05401a02
DimaIurusov <62394447+DimaIurusov@users.noreply.github.com>
2020-07-09 06:07:38 -0700

    То что я сделал

---
+++ Reception of medicines/Reception of medicines/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Reception of medicines
+//
+//  Created by StDismas on 09.07.2020.
+//  Copyright © 2020 StDismas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
