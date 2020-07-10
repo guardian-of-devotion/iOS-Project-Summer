f37d5c5ac20069c3ea64a8563e11ad5b6026c765
DimaIurusov <62394447+DimaIurusov@users.noreply.github.com>
2020-07-10 05:58:31 -0700

    Add files via upload

---
+++ 5 попытка/5 попытка/ProfileViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ProfileViewController.swift
+//  5 попытка
+//
+//  Created by StDismas on 10.07.2020.
+//  Copyright © 2020 StDismas. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ 5 попытка/5 попытка/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  5 попытка
+//
+//  Created by StDismas on 10.07.2020.
+//  Copyright © 2020 StDismas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
