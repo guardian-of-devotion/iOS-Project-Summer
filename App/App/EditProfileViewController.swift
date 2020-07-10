0188e939f10e541604009677926fdb29ae3b96ca
DimaIurusov <62394447+DimaIurusov@users.noreply.github.com>
2020-07-10 03:26:06 -0700

    Crash

---
+++ App/App/EditProfileViewController.swift
@@ -0,0 +1,24 @@
+//
+//  EditProfileViewController.swift
+//  App
+//
+//  Created by StDismas on 10.07.2020.
+//  Copyright © 2020 StDismas. All rights reserved.
+//
+
+import UIKit
+
+class EditProfileViewController: UIViewController {
+
+    @IBOutlet var Screen: UIView!
+    @IBOutlet weak var vButtonActoin: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+       
+    }
+    
+
+   	
+
+}
---
+++ App/App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  App
+//
+//  Created by StDismas on 10.07.2020.
+//  Copyright © 2020 StDismas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
